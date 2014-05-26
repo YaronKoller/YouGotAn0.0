959e16d62f668223b835b9bf7386252ca50f36d3
wuwoot9863 <wuwoot9863@hotmail.com>
2014-05-26 15:07:37 -0400

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Admin{
+  
+  private int employeeID;
+  private String password;
+  private String name;
+  
+  public Admin(int e, String p, String n){
+    employeeID = e;
+    password = p;
+    name = n;
+  }
+}
