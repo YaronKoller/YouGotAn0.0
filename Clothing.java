8900e4a5af0cb9f3bdf24a4e159a10c57ad1d9f7
wuwoot9863 <wuwoot9863@hotmail.com>
2014-05-26 15:06:15 -0400

    Create Clothing.java

---
+++ Clothing.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Clothing{
+  
+  private String type;
+  private String brand;
+  private String colour;
+  private String material;
+  
+  public Game(String t, String b, String c, String m){
+    type = t;
+    brand = b;
+    colour = c;
+    material = m;
+  }
+}
