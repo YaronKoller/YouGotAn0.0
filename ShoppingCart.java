3990087de5441a6375620b04773c5260af6cfc8b
wuwoot9863 <wuwoot9863@hotmail.com>
2014-05-26 15:07:03 -0400

    Create ShoppingCart.java

---
+++ ShoppingCart.java
@@ -0,0 +1,11 @@
+package project;
+
+public class ShoppingCart{
+  
+  private Clothing[] clothes;
+  private Game[] g;
+  private Food[] f;
+  
+  public void ShoppingCart(Clothing[] c,Game[] g, Food[] f){
+  }
+}
