44e8c2da2697c23fbef31251e3c9b0da89e4549a
wuwoot9863 <wuwoot9863@hotmail.com>
2014-05-26 15:06:33 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Game{
+  
+  private String title;
+  private String genre;
+  private String developer;
+  private String ageRestriction;
+  
+  public Game(String t, String g, String d, String a){
+    title = t;
+    genre = g;
+    developer = d;
+    ageRestriction = a;
+  }
+}
