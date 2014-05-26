25af6bfaaa75270b492e20c95551a2980a7774d4
yaronk27 <yaronkoller@live.ca>
2014-05-26 15:04:39 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Product{
+
+  private double price;
+  private int ID;
+  private int amount;
+  private int[] ratings;
+  
+  public Food(double p, int id, int a, int[] r){
+    price = p;
+    ID = id;
+    amount = a;
+    ratings = r;
+  }
+}
