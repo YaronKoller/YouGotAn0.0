9bf38c2f2f40100bab5b057d9b26c862acdd0009
yaronk27 <yaronkoller@live.ca>
2014-05-30 14:48:10 -0400

    Update Clothing.java

--- Clothing.java
+++ Clothing.java
@@ -7,10 +7,11 @@ public class Clothing{
   private String colour;
   private String material;
   
-  public Game(String t, String b, String c, String m){
+  public Game(String t, String b, String c, String m, double p, int id, int a){
     type = t;
     brand = b;
     colour = c;
     material = m;
+    supet(p, id, a);
   }
 }
