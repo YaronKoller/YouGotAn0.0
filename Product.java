10ce5b331a1c926ff35a6b64830d24889fabc8bc
yaronk27 <yaronkoller@live.ca>
2014-05-30 14:44:43 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -7,10 +7,48 @@ public class Product{
   private int amount;
   private int[] ratings;
   
-  public Food(double p, int id, int a, int[] r){
+  public Food(double p, int id, int a){
     price = p;
     ID = id;
     amount = a;
-    ratings = r;
+    ratings = new int[0];
+  }
+  
+  public void addSingle(){
+    amount++;
+  }
+  
+  public void addBulk(int n){
+    amount += n;
+  }
+  
+  public void remove(int n){
+    amount -= n;
+    
+    if(amount < 0)
+      amount = 0;
+  }
+  
+  public void addRating(int r){
+    int[] temp = new int[r.length+1];
+  }
+  
+  public double getPrice(){
+    return price;
+  }
+  
+  public int getID(){
+    return ID;
+  }
+  
+  public int getAmount(){
+    return amount;
+  }
+  
+  public double getAverageRating(){
+    int sum = 0;
+    for(int i=0; i<ratings.length; i++)
+      sum += ratings[i];
+    return 1.0*sum/ratings.length;
   }
 }
