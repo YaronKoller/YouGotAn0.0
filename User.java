16a5f8ab93c3e19f48c5e33520fae46d6da9ec8b
yaronk27 <yaronkoller@live.ca>
2014-05-31 17:21:53 -0400

    Update User.java

--- User.java
+++ User.java
@@ -11,5 +11,42 @@ public class User{
     email = e;
     password = p;
     name = n;
+    cart = new ShoppingCart();
+  }
+  
+  public boolean signIn(String pass){
+    if(pass.equals(password))
+      return true;
+    else
+      return false;
+  }
+  
+  public boolean modifyInfo(int choice, String newInfo){
+    if(choice=1){
+      email = newInfo;
+      return true;
+    }
+    else if(choice=2){
+      password = newInfo;
+      return true;
+    }
+    else if(choice=3){
+      name = newInfo;
+      return true;
+    }
+    else
+      return false;
+  }
+  
+  public String getEmail(){
+    return email;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public ShoppingCart getCart(){
+    return cart;
   }
 }
