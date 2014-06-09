f61f560308eff8db8fcce17ca1d46237294041e6
yaronk27 <yaronkoller@live.ca>
2014-06-09 02:03:18 -0400

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -6,11 +6,29 @@ public class ShoppingCart{
   private Food[] f;
   private Game[] g;
   
+  
   public ShoppingCart(){
     c = new Clothing[0];
     f = new Food[0];
     g = new Game[0];
   }
+  
+  
+  public boolean isEmpty(){
+    boolean empty = true;
+    
+    if(c.length > 0)
+      empty = false;
+    
+    if(f.length > 0)
+      empty = false;
+    
+    if(g.length > 0)
+      empty = false;
+    
+    return empty;
+  }
+  
   
   public void addProduct(Clothing cl){
     Clothing[] temp = new Clothing[c.length+1];
@@ -22,6 +40,7 @@ public class ShoppingCart{
     c = temp;
   }
   
+  
   public void addProduct(Food fo){
     Food[] temp = new Food[f.length+1];
     
@@ -31,6 +50,7 @@ public class ShoppingCart{
     temp[f.length] = fo;
     f = temp;
   }
+  
   
   public void addProduct(Game ga){
     Game[] temp = new Game[g.length+1];
@@ -42,26 +62,86 @@ public class ShoppingCart{
     g = temp;
   }
   
-  public String[] listAll(){
-    String list[] = new String[2];
+  
+  public int removeProduct(int ID){
+    int n = Search.clothes(c, ID);
+    if(n != -1){
+      Clothing[] C = new Clothing[c.length-1];
+      
+      for(int i=0; i<C.length; i++){
+        if(i<n)
+          C[i] = c[i];
+        else
+          C[i] = c[i+1];
+      }
+      c = C;
+      return 0;
+    }
     
-    list[0]="";
-    for(int i=0; i<(c.length-1); i++)
-      list[0] += (c[i].toString() + ",");
-    list[0] += c[c.length-1].toString();
+    n = Search.food(f, ID);
+    if(n != -1){
+      Food[] F = new Food[f.length-1];
+      
+      for(int i=0; i<F.length; i++){
+        if(i<n)
+          F[i] = f[i];
+        else
+          F[i] = f[i+1];
+      }
+      f = F;
+      return 0;
+    }
     
-    list[1]="";
-    for(int i=0; i<(f.length-1); i++)
-      list[1] += (f[i].toString() + ",");
-    list[1] += f[f.length-1].toString();
+    n = Search.games(g, ID);
+    if(n != -1){
+      Game[] G = new Game[g.length-1];
+      
+      for(int i=0; i<G.length; i++){
+        if(i<n)
+          G[i] = g[i];
+        else
+          G[i] = g[i+1];
+      }
+      g = G;
+      return 0;
+    }
     
-    list[2]="";
-    for(int i=0; i<(g.length-1); i++)
-      list[2] += (g[i].toString() + ",");
-    list[2] += g[g.length-1].toString();
-    
-    return list;
+    return 0/0;
   }
+  
+  
+  public void listAll(){
+    System.out.println("SHOPPING CART");
+    System.out.println();
+    
+    for(int i=0; i<c.length; i++)
+      System.out.println(c[i].toString());
+    
+    for(int i=0; i<f.length; i++)
+      System.out.println(f[i].toString());
+    
+    for(int i=0; i<g.length; i++)
+      System.out.println(g[i].toString());
+    
+    System.out.println();
+  }
+  
+  
+  public double totalPrice(){
+    double n = 0.0;
+    
+    for(int i=0; i<c.length; i++)
+      n += c[i].getPrice();
+    
+    for(int i=0; i<f.length; i++)
+      n += f[i].getPrice();
+    
+    for(int i=0; i<g.length; i++)
+      n += g[i].getPrice();
+    
+    return (Math.round(n*100))/100.0;
+  }
+  
   
   public void checkOut(){
     c = new Clothing[0];
